3efd89c9d3d1edabdc58e7e942f8ef7cba9d6879
Zinko <gabriel.marcelo.munoz@gmail.com>
2023-05-17 14:49:02 -0400

    añade ejer2

--- Tercer Semestre/LAB131/informe5/ejer2/src/App.java
+++ Tercer Semestre/LAB131/informe5/ejer2/src/App.java
@@ -1,45 +1,43 @@
 import java.util.*;
 
 public class App {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese el tamaño del vector: ");
-        int n = scanner.nextInt();
-        int[] vector = new int[n];
-
-        for (int i = 0; i < n; i++) {
-          System.out.print("Ingrese el elemento " + (i+1) + ": ");
-          vector[i] = scanner.nextInt();
-        }
-
-        System.out.println("\nLas posibles permutaciones son:");
-        permutaciones(vector, 0, n-1);
-      }
-      
-      public static void permutaciones(int[] vector, int primero, int ultimo) {
-        if (primero == ultimo) {
-          mostrar(vector);
-        } else {
-          for (int i = primero; i <= ultimo; i++) {
-            int aux = vector[primero];
-            vector[primero] = vector[i];
-            vector[i] = aux;
-            permutaciones(vector, primero + 1, ultimo);
-            aux = vector[primero];
-            vector[primero] = vector[i];
-            vector[i] = aux;
-          }
-        }
-      }
-      
-      public static void mostrar(int[] vector) {
-        System.out.print("{");
-        for (int i = 0; i < vector.length; i++) {
-          System.out.print(vector[i]);
-          if (i < vector.length-1) {
-            System.out.print(".");
-          }
-        }
-        System.out.println("}");
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Ingrese el tamaño del vector: ");
+    int n = scanner.nextInt();
+    int[] vector = new int[n];
+    for (int i = 0; i < n; i++) {
+      System.out.print("Ingrese el elemento " + (i + 1) + ": ");
+      vector[i] = scanner.nextInt();
+    }
+    System.out.println("\nLas posibles permutaciones son:");
+    permutaciones(vector, 0, n-1);
+  }
+    
+  public static void permutaciones(int[] vector, int primero, int ultimo) {
+    if (primero == ultimo) {
+      mostrar(vector);
+    } else {
+      for (int i = primero; i <= ultimo; i++) {
+        int aux = vector[primero];
+        vector[primero] = vector[i];
+        vector[i] = aux;
+        permutaciones(vector, primero + 1, ultimo);
+        aux = vector[primero];
+        vector[primero] = vector[i];
+        vector[i] = aux;
       }
     }
+  }
+    
+  public static void mostrar(int[] vector) {
+    System.out.print("{");
+    for (int i = 0; i < vector.length; i++) {
+      System.out.print(vector[i]);
+      if (i < vector.length-1) {
+        System.out.print(".");
+      }
+    }
+    System.out.println("}");
+  }
+}
