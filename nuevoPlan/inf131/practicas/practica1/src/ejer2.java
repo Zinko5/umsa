c55fc8337b9adaaa0425fa8c663954e899d750ba
Zinko <Zinko5omg@gmail.com>
2024-04-29 18:46:07 -0400

    empieza ejercicio 2

---
+++ nuevoPlan/inf131/practicas/practica1/src/ejer2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Queue;
+
+public interface ejer2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        /* int v[][] = new int[][3]; */
+        for (int i = 0; i < n; i++) {
+            
+            v[i][0] = sc.nextInt();
+            v[i][1] = sc.nextInt();
+            v[i][2] = sc.nextInt();
+        }
+    }
+}
