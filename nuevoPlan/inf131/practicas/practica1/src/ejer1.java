742126f187de09ba3f6e4317aac979a967af7664
Zinko <Zinko5omg@gmail.com>
2024-04-29 18:45:43 -0400

    termina ejercicio 1

--- nuevoPlan/inf131/practicas/practica1/src/ejer1.java
+++ nuevoPlan/inf131/practicas/practica1/src/ejer1.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ejer1 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int v[][] = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            v[i][0] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            int c = 0;
+            for (int j = 0; j < v[i][0]; j++) {
+                if ((v[i][0] + j) == (v[i][0] ^ j)) {
+                    c ++;
+                }
+            }
+            v[i][1] = c;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(v[i][1]);
+        }
+        sc.close();
     }
 }
