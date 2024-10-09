ed93dfb8120fdf56fc74aa4d4e37769c3527b5e7
Zinko <Zinko5omg@gmail.com>
2024-10-09 11:32:24 -0400

    reorganiza ejercicios del juez patito

---
+++ juezPatito/src/c2905e2.java
@@ -0,0 +1,43 @@
+// XOR en Numeros
+import java.util.Scanner;
+
+public class c2905e2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        for (int i = 0; i < t; i++) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long k = sc.nextLong();
+            System.out.println(kEsimoPos(a, b, k));
+        }
+        sc.close();
+    }
+
+    public static long kEsimoPos(long a, long b, long k) {
+        long lcmAb = (a * b) / gcd(a, b);
+        long left = 1, right = k * Math.min(a, b) * 2;
+
+        while (left < right) {
+            long mid = (left + right) / 2;
+            long count = (mid / a) + (mid / b) - 2 * (mid / lcmAb);
+            if (count < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    public static long gcd(long x, long y) {
+        while (y != 0) {
+            long temp = y;
+            y = x % y;
+            x = temp;
+        }
+        return x;
+    }
+}
---
+++ juezPatito/src/c2905e8.java
@@ -0,0 +1,54 @@
+// Lote especial
+import java.util.Scanner;
+
+public class c2905e8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int[] lote = new int[n];
+        for (int i = 0; i < n; i++) {
+            lote[i] = sc.nextInt();
+        }
+        boolean cond1 = true;
+        for (int i = 0; i < n - 1; i++) {
+            if (lote[i] > lote[i + 1]) {
+                cond1 = false;
+                break;
+            }
+        }
+        boolean cond2 = (sum(lote) % 2 == 1);
+        int contPrimos = 0;
+        for (int num : lote) {
+            if (esPrimo(num)) {
+                contPrimos++;
+            }
+        }
+        boolean cond3 = (contPrimos >= x);
+        if (cond1 && cond2 && cond3) {
+            System.out.println("SI");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    public static boolean esPrimo(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int sum(int[] array) {
+        int total = 0;
+        for (int num : array) {
+            total += num;
+        }
+        return total;
+    }
+}
