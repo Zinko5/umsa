0cc9b6198f8fe38b14a3f4640b87d3b79c906565
Zinko <gabriel.marcelo.munoz@gmail.com>
2024-10-13 20:59:06 -0400

    añade ejercicios del juez patito

---
+++ juezPatito/src/c2906e5.java
@@ -0,0 +1,53 @@
+// Ordenando funciones
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class c2906e5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        ArrayList<Integer> vector = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            vector.add(scanner.nextInt());
+        }
+        scanner.close();
+
+        for (int i = 0; i < vector.size() - 1; i++) {
+            for (int j = i + 1; j < vector.size(); j++) {
+                if (comparar(vector.get(i), vector.get(j)) > 0) {
+                    int temp = vector.get(i);
+                    vector.set(i, vector.get(j));
+                    vector.set(j, temp);
+                }
+            }
+        }
+
+        for (int i = 0; i < vector.size(); i++) {
+            System.out.print(vector.get(i));
+            if (i < vector.size() - 1) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static int sumarDigitos(int x) {
+        int suma = 0;
+        while (x != 0) {
+            suma += x % 10;
+            x /= 10;
+        }
+        return suma;
+    }
+
+    public static int comparar(int a, int b) {
+        int sdA = sumarDigitos(a);
+        int sdB = sumarDigitos(b);
+        if (sdA != sdB) {
+            return sdA - sdB;
+        } else {
+            return a - b;
+        }
+    }
+}
---
+++ juezPatito/src/c2906e6.java
@@ -0,0 +1,50 @@
+// Ordenando el Laboratorio
+import java.util.Scanner;
+
+public class c2906e6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int[][] a = new int[n][n];
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    a[j][k] = sc.nextInt();
+                }
+            }
+            boolean bienOrdenado = true;
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (a[j][k] != 1) {
+                        boolean encontrado = false;
+                        for (int s = 0; s < n; s++) {
+                            for (int u = 0; u < n; u++) {
+                                if (a[j][k] == a[j][s] + a[u][k]) {
+                                    encontrado = true;
+                                    break;
+                                }
+                            }
+                            if (encontrado) {
+                                break;
+                            }
+                        }
+                        if (!encontrado) {
+                            bienOrdenado = false;
+                            break;
+                        }
+                    }
+                }
+                if (!bienOrdenado) {
+                    break;
+                }
+            }
+            if (bienOrdenado) {
+                System.out.println("Si");
+            } else {
+                System.out.println("No");
+            }
+        }
+        sc.close();
+    }
+}
